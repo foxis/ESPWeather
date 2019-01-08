1e8ff6a040cf3cba07d695f03b87f514f7141e88
FoxIS <andrius.mikonis@gmail.com>
2019-01-08 15:07:58 +0200

    ADD: Maximum number of plot points. ADD: Loading previously collected data on startup for graphing.

--- client.py
+++ client.py
@@ -2,32 +2,7 @@
 
 This application subscribes to topics for each station and gathers data into
 a dictionary of dictionaries. Saves the data into a CSV file.
-
-
-usage: client.py [-h] [-c NAME] -u USERNAME -l PASSWORD -s SERVER [-p PORT]
-                 [-t TOPICS] [-a STATIONS]
-
-MQTT Client for Weather Station readings monitoring.
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -c NAME, --name NAME  MQTT Client Name (default P1)
-  -u USERNAME, --username USERNAME
-                        MQTT Client Username
-  -l PASSWORD, --password PASSWORD
-                        MQTT Client Password
-  -s SERVER, --server SERVER
-                        MQTT Server Address
-  -p PORT, --port PORT  MQTT Server Port
-  -t TOPICS, --topics TOPICS
-                        Comma separated list of topics to monitor (default
-                        temperature,battery,pressure,humidity,light)
-  -a STATIONS, --stations STATIONS
-                        Comma separated list of stations to monitor (default
-                        68:C6:3A:AC:3B:D4,DC:4F:22:18:F3:B7)
-  -f FILE, --file FILE  CSV file to write readings to (default {client name}.csv)
-  -v, --verbose         Verbose output to stdout
-  -g, --plot            Plot graphs of the readings
+Optionally displays the data.
   
 """
 
@@ -44,7 +19,7 @@ except :
     matplotlib_available = False
 
 class UserData(object):
-    def __init__(self, filename, topics, stations, verbose, plot):
+    def __init__(self, filename, topics, stations, verbose, plot, max_plot):
         self.filename = filename
         self.topics = topics
         self.stations = stations
@@ -53,8 +28,29 @@ class UserData(object):
         self.lock = thrd.Lock()
         self.readings = {station: {topic: [] for topic in topics} for station in stations}
         self.plots = None
+        self.max_plot = max_plot
+        self.default_plot = {station: {topic:[[], []] for topic in topics} for station in stations}
         self._connected = False
         self._last_connected = False
+
+        if os.path.exists(filename) and plot:
+            with open(filename) as f:
+                header = []
+                for i, line in enumerate(f.readlines()):
+                    data = line.split(',')
+                    if i == 0:
+                        header = data
+                    else:
+                        station = data[1]
+                        if station not in stations:
+                            continue
+                        
+                        for topic, val in zip(header[2:], data[2:]):
+                            if topic in topics:
+                                self.default_plot[station][topic][0] += [data[0]]
+                                self.default_plot[station][topic][1] += [float(val)]
+                                self.default_plot[station][topic][0] = self.default_plot[station][topic][0][-self.max_plot:]
+                                self.default_plot[station][topic][1] = self.default_plot[station][topic][1][-self.max_plot:]
 
     @property
     def connected(self):
@@ -104,17 +100,17 @@ class UserData(object):
             if write_headers:
                 header = "date,station,{}".format(",".join(self.topics))
                 print(header)
-                f.writelines([header])
+                f.writelines([header, "\n"])
             print(line)
-            f.writelines([line])
+            f.writelines([line, "\n"])
 
     def create_plots(self, station, topics):
         plots = dict(fig=plt.figure())
         N = len(topics)
         plots.update({
             topic: {
-                'x':[],
-                'y':[],
+                'x':self.default_plot[station][topic][0],
+                'y':self.default_plot[station][topic][1],
                 'plot':plots['fig'].add_subplot(N, 1, i + 1)
             } for i, topic in enumerate(topics)
         })
@@ -130,11 +126,11 @@ class UserData(object):
         if not self.plots:
             self.plots = {station: self.create_plots(station, self.topics) for station in self.stations}
 
-        self.plots[station][topic]['x'] += [datetime.now()]
+        self.plots[station][topic]['x'] += ["{}t".format(datetime.now())]
         self.plots[station][topic]['y'] += [float(data)]
 
-        self.plots[station][topic]['x'] = self.plots[station][topic]['x'][-30:]
-        self.plots[station][topic]['y'] = self.plots[station][topic]['y'][-30:]
+        self.plots[station][topic]['x'] = self.plots[station][topic]['x'][-self.max_plot:]
+        self.plots[station][topic]['y'] = self.plots[station][topic]['y'][-self.max_plot:]
 
         self.plots[station][topic]['plot'].clear()
         self.plots[station][topic]['plot'].plot(
@@ -166,9 +162,9 @@ def on_message(client, userdata, message):
         print("Exception occured: %s" % repr(e))
 
 
-def main(name, username, password, url, port, stations, topics, filename, verbose, plot):
+def main(name, username, password, url, port, stations, topics, filename, verbose, plot, max_plot):
 
-    userdata = UserData(filename, topics, stations, verbose, plot)
+    userdata = UserData(filename, topics, stations, verbose, plot, max_plot)
 
     client = mqtt.Client(name, userdata=userdata)
     client.username_pw_set(username, password)
@@ -197,31 +193,34 @@ if __name__ == "__main__":
     stations = "68:C6:3A:AC:3B:D4,DC:4F:22:18:F3:B7"
 
     parser = argparse.ArgumentParser(description="MQTT Client for Weather Station readings monitoring.")
-    parser.add_argument("-c", "--name", default="P1",
-                        help="MQTT Client Name (default P1)")
-    parser.add_argument("-u", "--username", required=True,
-                        help="MQTT Client Username")
-    parser.add_argument("-l", "--password", required=True, 
-                        help="MQTT Client Password")
-    parser.add_argument("-s", "--server", required=True,
+    parser.add_argument("server", 
                         help="MQTT Server Address")
+    parser.add_argument("name", 
+                        help="MQTT Client Name (default P1)")
+    parser.add_argument("-u", "--username", 
+                        help="MQTT Client Username")
+    parser.add_argument("-l", "--password", 
+                        help="MQTT Client Password")
     parser.add_argument("-p", "--port", type=int, default=16769,
                         help="MQTT Server Port")
     parser.add_argument("-t", "--topics", default=topics,
                         help="Comma separated list of topics to monitor (default %s)" % topics)
-    parser.add_argument("-a", "--stations", default=stations,
+    parser.add_argument("-s", "--stations", default=stations,
                         help="Comma separated list of stations to monitor (default %s)" % stations)
     parser.add_argument("-f", "--file", default=None,
                         help="CSV file to write readings to (default {client name}.csv)")
     parser.add_argument("-v", "--verbose", const=True, default=False, action='store_const',
                         help="Verbose output to stdout")
-    parser.add_argument("-g", "--plot", const=matplotlib_available, default=False, action='store_const',
+    parser.add_argument("-P", "--plot", const=matplotlib_available, default=False, action='store_const',
                         help="Plot graphs of the readings (matplotlib importable=%s)"%str(matplotlib_available))
+    parser.add_argument("-n", "--max_plot", default=120, 
+                        help="Maximum number of points to plot (default 120)")
     args = parser.parse_args()
 
     filename = args.file if args.file else "{}.csv".format(args.name)
 
     main(args.name, args.username, args.password,
          args.server, args.port, args.stations.split(','),
-         args.topics.split(','), filename, args.verbose, args.plot)
+         args.topics.split(','), filename, args.verbose,
+         args.plot, args.max_plot)
     
